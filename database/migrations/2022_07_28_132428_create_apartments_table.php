7169094813c37353ad65f1e5bbdebe361c5f9518
Slobodan Maric <slobodan.maric85@gmail.com>
2022-07-28 15:43:42 +0200

    Added migrations for apratments and categories

---
+++ database/migrations/2022_07_28_132428_create_apartments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id')->index();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->string('name');
+            $table->float('price', 10);
+            $table->enum('currency', ['EUR', 'USD', 'BAM'])->default('EUR');
+            $table->text('description')->nullable();
+            $table->json('properties')->nullable();
+            $table->float('rating')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
