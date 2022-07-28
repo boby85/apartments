32420d937be75d393a529b174d74c3ffea62e55e
Slobodan Maric <slobodan.maric85@gmail.com>
2022-07-28 17:44:38 +0200

    Model bindings

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Apartment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name", "price", "currency", "description", "properties", "rating"];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
